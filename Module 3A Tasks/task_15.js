02226c7a341f2316cd35888bb8cf92224aff101e
cimmy12 <tomismith67@gmail.com>
2023-07-19 19:02:10 +0100

    Books Array

--- Module 3A Tasks/task_15.js
+++ Module 3A Tasks/task_15.js
@@ -1,32 +1,36 @@
-const books =[ 
-    {
-    title:'Learning JS level 1', 
-    description:'Learning JS at beginner level', 
-    numberOfPages:50, 
-    author:'Simi Adanri', 
-    reading: true,
-    },
 
-    {
-    title:'Learn JS Intermediate level ', 
-    description:'Learning JS at intermediate level', 
-    numberOfPages:40, 
-    author:'Simi Adanri', 
-    reading: true,
-    },
-{
-    title:'Advanced JS', 
-    description:'Learning JS at Advanced level', 
-    numberOfPages:30, 
-    author:'Simi Adanri', 
-    reading: false,
-},
-]
-
-for (let i=0; i < books.length;i++)
-if (books.reading===true){
-    console.log('Learn JS')
-}
-
+const books ={
+    booksArray:[{
+            title:'Learning JS level 1', 
+            description:'Learning JS at beginner level', 
+            numberOfPages:50, 
+            author:'Simi Adanri', 
+            reading: true,
+            },
+        
+            {
+            title:'Learn JS Intermediate level ', 
+            description:'Learning JS at intermediate level', 
+            numberOfPages:40, 
+            author:'Simi Adanri', 
+            reading: true,
+            },
+        {
+            title:'Advanced JS', 
+            description:'Learning JS at Advanced level', 
+            numberOfPages:30, 
+            author:'Simi Adanri', 
+            reading: false,
+        }
+        ],
+        
+        }
+        for (let i=0; i < books.length;i++){
+            console.log(books[i]);
+        if (books[i].reading===true){
+            break;
+        }
+    }    
+        console.log(books.booksArray[0,1])
 
 //
