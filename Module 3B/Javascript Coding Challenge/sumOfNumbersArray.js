76f4782325bca7c9fed8feefd98bc7d4401bfa3a
cimmy12 <tomismith67@gmail.com>
2023-07-19 23:59:29 +0100

    JS challenge

---
+++ Module 3B/Javascript Coding Challenge/sumOfNumbersArray.js
@@ -0,0 +1,10 @@
+//Calculate the sum of numbers within an array.
+
+
+function sumNumbers(firstNumber, secondNumber, thirdNumber){
+    const sum =firstNumber + secondNumber +thirdNumber;
+    return [sum];
+}
+
+console.log(sumNumbers(12,16,22))
+
